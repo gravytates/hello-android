40272a86ddfb4bd4f7c6632d88cfc98a29476911
grady shelton <gradyish@gmail.com>
2018-05-24 13:51:16 -0700

    add text edit field and log response upon button click,. use intent to transition to FoodActivity.

--- app/src/main/java/com/gradyshelton/foodnsuch/MainActivity.java
+++ app/src/main/java/com/gradyshelton/foodnsuch/MainActivity.java
@@ -1,13 +1,18 @@
 package com.gradyshelton.foodnsuch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindFoodButton;
+    private EditText mFoodEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +20,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFindFoodButton = (Button) findViewById(R.id.findFoodButton);
+        mFoodEditText = (EditText) findViewById(R.id.foodEditText);
         mFindFoodButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello Android!", Toast.LENGTH_LONG).show();
+                String food = mFoodEditText.getText().toString();
+                Log.d(TAG, food);
+                Intent intent = new Intent(MainActivity.this, FoodActivity.class);
+                startActivity(intent);
             }
         });
     }
