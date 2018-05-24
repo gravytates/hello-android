1e4691cee9a88be7e43d79f82fb1e8fc814bea22
grady shelton <gradyish@gmail.com>
2018-05-24 12:54:22 -0700

    add clickListener and new FoodActivity

--- app/src/main/java/com/gradyshelton/foodnsuch/MainActivity.java
+++ app/src/main/java/com/gradyshelton/foodnsuch/MainActivity.java
@@ -2,12 +2,26 @@ package com.gradyshelton.foodnsuch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindFoodButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindFoodButton = (Button) findViewById(R.id.findFoodButton);
+        mFindFoodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello Android!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
+
+
