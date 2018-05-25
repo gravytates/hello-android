ba6af8ebe9d1ba96f924c5cc7d5d862fe4470f29
grady shelton <gradyish@gmail.com>
2018-05-25 13:07:41 -0700

    add first ui test using robolectric. add custom font for home  screen

--- app/src/main/java/com/gradyshelton/foodnsuch/MainActivity.java
+++ app/src/main/java/com/gradyshelton/foodnsuch/MainActivity.java
@@ -1,12 +1,14 @@
 package com.gradyshelton.foodnsuch;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String fontPath = "fonts/Roboto-ThinItalic.ttf";
+        TextView mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface robotoFont = Typeface.createFromAsset(getAssets(), fontPath);
+        mAppNameTextView.setTypeface(robotoFont);
 
         mFindFoodButton = (Button) findViewById(R.id.findFoodButton);
         mFoodEditText = (EditText) findViewById(R.id.foodEditText);
