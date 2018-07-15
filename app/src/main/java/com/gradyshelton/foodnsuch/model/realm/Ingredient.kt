ef441a1336ff23fe06f46d2db6abf76cfb6dac26
grady shelton <gradyish@gmail.com>
2018-07-14 17:15:50 -0700

    clean up ingredient model, deleting comments and making the name property required

--- app/src/main/java/com/gradyshelton/foodnsuch/model/realm/Ingredient.kt
+++ app/src/main/java/com/gradyshelton/foodnsuch/model/realm/Ingredient.kt
@@ -1,6 +1,5 @@
 package com.gradyshelton.foodnsuch.model.realm
 
-//import com.google.gson.annotations.SerializedName
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 import io.realm.annotations.Required
@@ -9,12 +8,8 @@ open class Ingredient : RealmObject {
     @PrimaryKey
     @Required
     var id: String = ""
+    @Required
     var name: String = ""
     var price: Double = 0.0
     constructor()
 }
-
-//open class Ingredient(@SerializedName(value = "name") var name: String,
-//                      @SerializedName(value = "price") var price: Double) : RealmObject() {
-//    constructor() : this("", 0.0)
-//}
