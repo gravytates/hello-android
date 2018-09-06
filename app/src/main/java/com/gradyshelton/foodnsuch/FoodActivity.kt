9eb96f11df830605b9707b6815f6420456117954
grady shelton <gradyish@gmail.com>
2018-09-06 14:09:04 -0700

    change array to mutable list so that food list can be added to

--- app/src/main/java/com/gradyshelton/foodnsuch/FoodActivity.kt
+++ app/src/main/java/com/gradyshelton/foodnsuch/FoodActivity.kt
@@ -2,14 +2,16 @@ package com.gradyshelton.foodnsuch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class FoodActivity : AppCompatActivity() {
     private var mFoodTextView: TextView? = null
-    private var ingredients = arrayOf<String>("Avocado", "Garlic", "Lime", "Salt", "Pepper", "Onion", "Chicken", "Beef", "Cheese", "Red Wine", "basil", "whiskey", "potato", "tomato", "lettuce")
+    private var ingredients: MutableList<String> = mutableListOf("Avocado", "Garlic", "Lime", "Salt", "Pepper", "Onion", "Chicken", "Beef", "Cheese", "Red Wine", "basil", "whiskey", "potato", "tomato", "lettuce")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +19,11 @@ class FoodActivity : AppCompatActivity() {
         val mIngredientListView = findViewById<ListView>(R.id.ingredientListView)
         val mInfoTextView = findViewById<TextView>(R.id.infoTextView)
         val mUserInput: String = intent.getStringExtra(EXTRA_MESSAGE)
-        val addedIngredients = ingredients.plus(mUserInput)
+        val addedIngredients = ingredients
+
+        if (mUserInput != "") {
+            addedIngredients.add(mUserInput)
+        }
 
         val adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, addedIngredients)
         mIngredientListView.setAdapter(adapter)
